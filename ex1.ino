a86fbb10f77432a5ef2c305983f4ef8599ef9c35
happyg920624 <51997834+happyg920624@users.noreply.github.com>
2019-06-27 14:56:31 +0800

    Add files via upload

---
+++ ex1.ino
@@ -0,0 +1,24 @@
+/*
+ * Generated using BlocklyDuino:
+ *
+ * https://github.com/MediaTek-Labs/BlocklyDuino-for-LinkIt
+ *
+ * Date: Mon, 03 Jun 2019 12:14:50 GMT
+ */
+
+
+
+void setup()
+{
+
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+
+void loop()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+}
